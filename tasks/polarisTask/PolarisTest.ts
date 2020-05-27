41b8a6c59face2a116739a4316d49eb418446eb7
Jordan Piscitelli <jordanp@synopsys.com>
2020-05-27 09:52:34 -0400

    fix: Using a fs-extra version compatible with the azure node version.

--- tasks/polarisTask/PolarisTest.ts
+++ tasks/polarisTask/PolarisTest.ts
@@ -43,7 +43,7 @@ async function run() {
         var polaris_actions = new PolarisActions(log, tl);
 
         var target = tl.cwd();
-        var scan_cli_json_path = await polaris_actions.execute(polaris_url, polaris_token, target, polaris_client, build_command);
+        var scan_cli_json_path = await polaris_actions.execute(polaris_url, polaris_token, target, target, polaris_client, build_command);
 
         log.debug("Executed polaris.");
 
