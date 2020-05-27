8cdcdc2bfe8954abb20694d61b6b604e6f6676ad
Jordan Piscitelli <jordanp@synopsys.com>
2020-05-27 09:44:55 -0400

    fix: Using actual tools directory, cleaning previous installs and using version file.

--- tasks/polarisTask/PolarisActions.ts
+++ tasks/polarisTask/PolarisActions.ts
@@ -6,6 +6,7 @@ const fs = require('fs');
 const path = require('path');
 const zipper = require('adm-zip');
 const json_path = require('jsonpath');
+const fse = require('fs-extra');
 
 import PolarisClient from "./PolarisClient"
 import PolarisUtility from "./PolarisUtility"
@@ -19,12 +20,12 @@ export default class PolarisActions {
         this.log = log;
     }
 
-    async execute(polaris_url: string, polaris_token:string, polaris_target:string, polaris_client: PolarisClient, build_command: string): Promise<String> {
+    async execute(polaris_url: string, polaris_token:string, polaris_target:string, polaris_install_path:string, polaris_client: PolarisClient, build_command: string): Promise<String> {
         this.log.info("Starting polaris task.")
         var polaris_cli_name = "polaris"; // used to be "swip"
         var polaris_utility = new PolarisUtility(this.log);
     
-        var polaris_cli_location = path.resolve(__dirname, "polaris");
+        var polaris_cli_location = path.resolve(polaris_install_path, "polaris");
         var version_file = path.join(polaris_cli_location, "version.txt");
         var relative_cli_url = polaris_utility.determine_cli_relative_location(polaris_cli_name);
         var cli_url = polaris_url + relative_cli_url;
@@ -42,8 +43,10 @@ export default class PolarisActions {
             this.log.debug("Available version: " + available_version_date.format())
             if (current_version_date.isBefore(available_version_date)) {
                 this.log.info("Downloading Polaris CLI because a newer version is available.")
-                fs.writeFileSync(version_file, available_version_date.format());
                 download_cli = true;
+                this.log.info(`Cleaning up the Polaris installation directory: ${polaris_cli_location}`);
+                this.log.info("Please do not place anything in this folder, it is under extension control.");
+                fse.removeSync(polaris_cli_location);
             } else {
                 this.log.info("Existing Polaris CLI will be used.")
             }
@@ -54,11 +57,13 @@ export default class PolarisActions {
     
         if (download_cli) {
             this.log.info("Starting download.")
-            const polaris_zip = path.resolve(__dirname, "polaris.zip");
+            const polaris_zip = path.join(polaris_install_path, "polaris.zip");
             await polaris_client.download_cli(cli_url, polaris_zip);
             this.log.info("Starting extraction.")
             await polaris_utility.extract_cli(polaris_zip,  polaris_cli_location);
             this.log.info("Download and extraction finished.")
+            fs.writeFileSync(version_file, available_version_date.format());
+            this.log.info(`Wrote version file: ${version_file}`)
         }
         
         this.log.info("Looking for Polaris executable.")
--- tasks/polarisTask/PolarisTask.ts
+++ tasks/polarisTask/PolarisTask.ts
@@ -36,7 +36,16 @@ async function run() {
         }
 
         log.debug(`Read task configuration: ${polaris_url} @ ${polaris_token}`);
-        
+
+        let polaris_install_path: string | undefined;
+        polaris_install_path = tl.getVariable('Agent.ToolsDirectory');
+        if (!polaris_install_path) {
+            log.warn("Agent did not have a tool directory, polaris will be installed to the current working directory.");
+            polaris_install_path = tl.cwd();
+        }
+
+        log.into(`Installing polaris to directory: ` + polaris_install_path);
+
         var polaris_client = new PolarisClient(log, polaris_url, polaris_token);
         await polaris_client.authenticate();
 
@@ -45,7 +54,7 @@ async function run() {
         var polaris_actions = new PolarisActions(log, tl);
 
         var target = tl.cwd();
-        var scan_cli_json_path = await polaris_actions.execute(polaris_url, polaris_token, target, polaris_client, build_command);
+        var scan_cli_json_path = await polaris_actions.execute(polaris_url, polaris_token, target, polaris_install_path, polaris_client, build_command);
 
         log.debug("Executed polaris.");
 
