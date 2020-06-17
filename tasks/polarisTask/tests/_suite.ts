30516e348b1c583862a3134da8a8061778d86691
Jordan Piscitelli <jordanp@synopsys.com>
2020-06-17 17:35:24 -0400

    test: Adding simple test following microsoft example.

---
+++ tasks/polarisTask/tests/_suite.ts
@@ -0,0 +1,25 @@
+import * as path from 'path';
+import * as assert from 'assert';
+import * as ttm from 'azure-pipelines-task-lib/mock-test';
+
+describe('Sample task tests', function () {
+
+    before( function() {
+
+    });
+
+    after(() => {
+
+    });
+
+    it('should succeed with simple inputs', function(done: MochaDone) {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'success.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+    
+        tr.run();
+        assert.equal(tr.succeeded, false, 'should have failed');
+        done();
+    });
+});
